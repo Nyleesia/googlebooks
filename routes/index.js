07fecc1483a62523dd6574f4ab9fdac0ab2ea267
Nyleesia Nicome Schnarr <nyleesia.nicome@users.noreply.github.com>
2019-11-12 14:43:39 -0500

    added crud routes

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = require("express").Router();
+const controller = require("../../controller");
+
+router.get(
+  "/", (req, res) => {
+    controller.findAllBooks (req, res)
+}
+); 
+
+router.get(
+  "/:id", (req, res) => {
+    controller.findBookById (req, res)
+}
+); 
+
+router.post(
+  "/", (req, res) => {
+    controller.saveBook (req, res)
+}
+); 
+
+router.put(
+  "/:id", (req, res) => {
+    controller.updateBook (req, res)
+}
+); 
+
+router.delete(
+  "/:id", (req, res) => {
+    controller.deleteBook (req, res)
+}
+); 
+
+module.exports = router;
