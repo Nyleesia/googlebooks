9131714e2ddd93614486dccb87d5b91565a92a8b
Nyleesia Nicome Schnarr <nyleesia.nicome@users.noreply.github.com>
2019-11-12 15:13:34 -0500

    navbar updated

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./style.css";
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
-    <div className = "navbar navbar-dark bg-dark">
-      <a href="/googlebooks"><span className= "gameName">{props.nameHREF}</span> </a>
-      {/* <span className = "score"> {`Score: ${props.score}`} </span>
-      <span className = "highscore"> {`High Score: ${props.highscore}`} </span> */}
+    <div id = "navbar">
+      <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">
+                <h2 className="text-white">Google API Book Search</h2>
+            </a>
+            <div id="navBar">
+                <ul className="navbar-nav">
+                    <li className="nav-item" id="home">
+                        <a className="nav-link" href="/">
+                            <button type="button" className="btn btn-info text-white">Search</button>
+                        </a>
+                    </li>
+                    <li className="nav-item" id="library">
+                        <a className="nav-link" href="/library">
+                            <button type="button" className="btn btn-warning text-white">Library</button>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
   )
 }
